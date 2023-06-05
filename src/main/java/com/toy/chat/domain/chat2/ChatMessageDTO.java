2b97b6cd3e632fa31a03c0ec978f151c8e3a8eca
YHLEE9753 <dldydgns530@gmail.com>
2023-06-05 16:50:53 +0900

    feat: stomp web socket chat

--- src/main/java/com/toy/chat/domain/chat2/ChatMessageDTO.java
+++ src/main/java/com/toy/chat/domain/chat2/ChatMessageDTO.java
@@ -1,4 +1,13 @@
 package com.toy.chat.domain.chat2;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ChatMessageDTO {
+
+    private String roomId;
+    private String writer;
+    private String message;
 }
