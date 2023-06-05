d37e594b74505495a0ae6b19285a2553c18be871
YHLEE9753 <dldydgns530@gmail.com>
2023-06-05 14:10:46 +0900

    init

---
+++ src/test/java/com/toy/chat/ChatApplicationTests.java
@@ -0,0 +1,15 @@
+package com.toy.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ChatApplicationTests{
+
+	@Test
+	public void test(){
+		// test
+	}
+}
