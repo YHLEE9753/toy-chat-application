fc0798e37e5932d0fd8b8f6a554793fadd4d9cfa
YHLEE9753 <dldydgns530@gmail.com>
2023-06-06 18:41:41 +0900

    fix: querydsl 테스트 오류 수정(미완)

--- src/test/java/com/toy/chat/config/ServiceTestConfig.java
+++ src/test/java/com/toy/chat/config/ServiceTestConfig.java
@@ -5,10 +5,11 @@ import com.toy.chat.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 @DataJpaTest
+@Import(QuerydslTestConfig.class)
 public abstract class ServiceTestConfig extends MySqlContainerSetting {
-
     @Autowired
     protected MemberRepository memberRepository;
 
